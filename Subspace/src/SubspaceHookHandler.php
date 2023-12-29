d4034bae352fc05e461bdca269ae83442f4ac366
Clay Salvage <cle@wongery.com>
2023-12-29 06:42:04 -0800

    Fixed error (see https://www.wongery.com/blog/20231229)

--- Subspace/src/SubspaceHookHandler.php
+++ Subspace/src/SubspaceHookHandler.php
@@ -7,9 +7,10 @@ use MediaWiki\MediaWikiServices;
 use NamespaceInfo;
 use Title;
 use Article;
+use WikiPage;
 // use Wikimedia\Rdbms\Subquery;
 
-class SubspaceHookHandler implements \MediaWiki\Page\Hook\ArticleFromTitleHook
+class SubspaceHookHandler implements \MediaWiki\Page\Hook\ArticleFromTitleHook, \MediaWiki\Page\Hook\WikiPageFactoryHook
 {
 	public function onArticleFromTitle($title, &$article, $context)
 	{
@@ -49,8 +50,37 @@ class SubspaceHookHandler implements \MediaWiki\Page\Hook\ArticleFromTitleHook
 				// var_dump($newTitle);
 				// $newarticle = Article::newFromTitle($newTitle, $context);
 				$article = new Article($newTitle);
-				// var_dump($newarticle);
-				return true;
+				// var_dump($article);
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public function onWikiPageFactory($title, &$page)
+	{
+		if ($title->mArticleID !== -1) return true;
+		if (!str_contains($title->getBaseText(), ":")) return true;
+		$explodedTitle = explode('#', $title->getFullText(), 2);
+		$explodedTitle2 = explode('/', $explodedTitle[0], 2);
+		$baseTitle = $explodedTitle2[0];
+		$subpage = count($explodedTitle2) > 1 ? $explodedTitle2[1] : "";
+		$anchor = count($explodedTitle) > 1 ? $explodedTitle[1] : "";
+		$titleParts = explode(":", $baseTitle);
+
+
+		$services = MediaWikiServices::getInstance();
+		$namespaceInfo = $services->getNameSpaceInfo();
+		for ($i = count($titleParts) - 1; $i > 0; $i--) {
+			$namespace = implode(':', array_slice($titleParts, 0, $i));
+			if (($ns = $namespaceInfo->getCanonicalIndex(strtolower(str_replace(":", "_", $namespace)))) !== null) {
+				$pageTitle = implode(':', array_slice($titleParts, $i));
+				if ($subpage !== "") $pageTitle .= "/" . $subpage;
+				$newTitle = Title::makeTitle($ns, $pageTitle, $anchor);
+				$page = new WikiPage($newTitle);
+				// var_dump($page);
+				return false;
 			}
 		}
 
